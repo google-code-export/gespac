8396c182b5fee9d7347cdca3e3183303ed10f9d7
ati.gastondefferre <ati.gastondefferre@98ed3dca-a519-a6af-8faf-6aacfb961b17>
2010-11-18 16:18:12 +0000

    pour modifier son propre compte

--- gespac33/gespac/gestion_utilisateurs/form_utilisateur_personnel.php
+++ gespac33/gespac/gestion_utilisateurs/form_utilisateur_personnel.php
@@ -1,19 +1,16 @@
 <?PHP
-
-	#formulaire d'ajout et de modification
-	#des users !
-
-
+session_start();
+	
+	#formulaire de modification de son propre compte
 
 	header("Content-Type:text/html; charset=iso-8859-1" ); 	// règle le problème d'encodage des caractères
 
 	include ('../config/databases.php');	// fichiers de configuration des bases de données
 	include ('../config/pear.php');			// fichiers de configuration des lib PEAR (setinclude + packages)
 
-?>
+	$login =  $_SESSION['login'];
 
-<!--  SERVEUR AJAX -->
-<script type="text/javascript" src="server.php?client=all"></script>
+?>
 
 <script type="text/javascript"> 
 	
@@ -22,10 +19,9 @@
 
 		var bt_submit 	  = $("post_user");
 		var user_nom 	  = $("nom").value;
-		var user_login 	  = $("login").value;
 		var user_password = $("password").value;
 		
-		if (user_nom == "" || user_login == "" || user_password == "") {
+		if (user_nom == "" || user_password == "") {
 			bt_submit.disabled = true;
 		} else {
 			bt_submit.disabled = false;
@@ -51,7 +47,7 @@
 				onSuccess: function(responseText, responseXML, filt) {
 					$('target').set('html', responseText);
 					$('conteneur').set('load', {method: 'post'});	//On change la methode d'affichage de la page de GET à POST (en effet, avec GET il récupère la totalité du tableau get en paramètres et lorsqu'on poste la page formation on dépasse la taille maxi d'une url)
-					window.setTimeout("$('conteneur').load('gestion_utilisateurs/voir_utilisateurs.php');", 1500);
+					window.setTimeout("$('conteneur').load('index.php');", 1500);
 					SexyLightbox.close();
 				}
 			
@@ -61,6 +57,7 @@
 	
 </script>
 
+
 <?PHP
 
 	// adresse de connexion à la base de données
@@ -68,21 +65,15 @@
 
 	// cnx à la base de données GESPAC
 	$db_gespac 	= & MDB2::factory($dsn_gespac);
-	
-	$id = $_GET['id'];
 
-		
 		
 		#***************************************************************************
 		# 				MODIFICATION de l'utilisateur
 		#***************************************************************************
 		
 		
-	
-		echo "<h2>formulaire de modification d'un utilisateur</h2><br>";
-		
 		// Requete pour récupérer les données des champs pour le user à modifier
-		$user_a_modifier = $db_gespac->queryAll ( "SELECT user_id, user_nom, user_logon, user_password, user_niveau, user_mail, user_skin, user_accueil FROM users WHERE user_id=$id" );		
+		$user_a_modifier = $db_gespac->queryAll ( "SELECT user_id, user_nom, user_logon, user_password, user_niveau, user_mail, user_skin, user_accueil, user_mailing FROM users WHERE user_logon='$login'" );		
 		
 		// valeurs à affecter aux champs
 		$user_id 			= $user_a_modifier[0][0];
@@ -93,7 +84,11 @@
 		$user_mail 			= $user_a_modifier[0][5];
 		$user_skin 			= $user_a_modifier[0][6];
 		$user_accueil		= $user_a_modifier[0][7];
+		$user_mailing		= $user_a_modifier[0][8];
+		
+		$checked = $user_mailing == 1 ? "checked" : "";
 	
+		echo "<h2>formulaire de modification de $user_nom</h2><br>";
 		
 		?>
 		
@@ -102,19 +97,13 @@
 			$('nom').focus();
 		</script>
 		
-		<form action="gestion_utilisateurs/post_utilisateurs.php?action=mod" method="post" name="post_form" id="post_form">
-			<input type=hidden name=id value=<?PHP echo $user_id;?> >
+		<form action="gestion_utilisateurs/post_utilisateur_personnel.php" method="post" name="post_form" id="post_form">
 			<center>
 			<table width=500>
 			
 				<tr>
 					<TD>Nom</TD>
 					<TD><input type=text name=nom id=nom value= "<?PHP echo $user_nom; ?>" 	/></TD>
-				</tr>
-				
-				<tr>
-					<TD>log In</TD>
-					<TD><input type=text name=login value= "<?PHP echo $user_logon; ?>"	/></TD>
 				</tr>
 				
 				<tr>
@@ -125,6 +114,11 @@
 				<tr>
 					<TD>Mail</TD> 
 					<TD><input type=text name=mail value= "<?PHP echo $user_mail; ?>"	/></TD>
+				</tr>
+				
+				<tr>
+					<TD>Mailing</TD> 
+					<TD><input type=checkbox name=mailing <?PHP echo $checked;?>	/></TD>
 				</tr>
 				
 				<tr>
@@ -143,17 +137,6 @@
 						?>
 						</select>
 					</td>
-				</tr>
-				
-				<tr style="display:none">
-					<TD>Niveau</TD>
-						<TD><select name="niveau" size="1">
-							<option <?PHP if ( $user_niveau == 1 ) echo "selected"; ?> value=1>ATI</option>
-							<option <?PHP if ( $user_niveau == 2 ) echo "selected"; ?> value=2>TICE</option>
-							<option <?PHP if ( $user_niveau == 3 ) echo "selected"; ?> value=3>Professeur</option>
-							<option <?PHP if ( $user_niveau == 9 ) echo "selected"; ?> value=9>Autre...</option>
-						</select>
-					</TD>
 				</tr>
 				
 				<tr>
@@ -176,12 +159,12 @@
 			</table>
 			
 			<br>
-			<input type=submit value='Modifier cet utilisateur' >
+			<input type=submit value='Modifier mon compte' >
 
 			</center>
 
 		</FORM>
 		
-
+		
 <!--	DIV target pour Ajax	-->
 <div id="target"></div>
