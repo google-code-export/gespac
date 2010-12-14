d38ee55bbd155ebabfe95a7f8494c962a5981135
ati.gastondefferre@gmail.com <ati.gastondefferre@gmail.com@98ed3dca-a519-a6af-8faf-6aacfb961b17>
2010-12-14 07:09:39 +0000

    blocage de la modification du compte root dans "modifier mon compte"

--- gespac33/gespac/gestion_utilisateurs/form_utilisateur_personnel.php
+++ gespac33/gespac/gestion_utilisateurs/form_utilisateur_personnel.php
@@ -60,110 +60,120 @@ session_start();
 
 <?PHP
 
-	// adresse de connexion à la base de données
-	$dsn_gespac     = 'mysql://'. $user .':' . $pass . '@localhost/' . $gespac;
+	if ( $login <> "ati" ) {
 
-	// cnx à la base de données GESPAC
-	$db_gespac 	= & MDB2::factory($dsn_gespac);
+		// adresse de connexion à la base de données
+		$dsn_gespac     = 'mysql://'. $user .':' . $pass . '@localhost/' . $gespac;
 
-		
-		#***************************************************************************
-		# 				MODIFICATION de l'utilisateur
-		#***************************************************************************
-		
-		
-		// Requete pour récupérer les données des champs pour le user à modifier
-		$user_a_modifier = $db_gespac->queryAll ( "SELECT user_id, user_nom, user_logon, user_password, user_mail, user_skin, user_accueil, user_mailing FROM users WHERE user_logon='$login'" );		
-		
-		// valeurs à affecter aux champs
-		$user_id 			= $user_a_modifier[0][0];
-		$user_nom	 		= $user_a_modifier[0][1];
-		$user_logon	 		= $user_a_modifier[0][2];
-		$user_password 		= $user_a_modifier[0][3];
-		$user_mail 			= $user_a_modifier[0][4];
-		$user_skin 			= $user_a_modifier[0][5];
-		$user_accueil		= $user_a_modifier[0][6];
-		$user_mailing		= $user_a_modifier[0][7];
-		
-		$checked = $user_mailing == 1 ? "checked" : "";
-	
-		echo "<h2>formulaire de modification de $user_nom</h2><br>";
-		
-		?>
-		
-		<script>
-			// Donne le focus au premier champ du formulaire
-			$('nom').focus();
-		</script>
-		
-		<form action="gestion_utilisateurs/post_utilisateur_personnel.php" method="post" name="post_form" id="post_form">
-			<center>
-			<table width=500>
+		// cnx à la base de données GESPAC
+		$db_gespac 	= & MDB2::factory($dsn_gespac);
+
 			
-				<tr>
-					<TD>Nom</TD>
-					<TD><input type=text name=nom id=nom value= "<?PHP echo $user_nom; ?>" 	/></TD>
-				</tr>
+			#***************************************************************************
+			# 				MODIFICATION de l'utilisateur
+			#***************************************************************************
+			
+			
+			// Requete pour récupérer les données des champs pour le user à modifier
+			$user_a_modifier = $db_gespac->queryAll ( "SELECT user_id, user_nom, user_logon, user_password, user_mail, user_skin, user_accueil, user_mailing FROM users WHERE user_logon='$login'" );		
+			
+			// valeurs à affecter aux champs
+			$user_id 			= $user_a_modifier[0][0];
+			$user_nom	 		= $user_a_modifier[0][1];
+			$user_logon	 		= $user_a_modifier[0][2];
+			$user_password 		= $user_a_modifier[0][3];
+			$user_mail 			= $user_a_modifier[0][4];
+			$user_skin 			= $user_a_modifier[0][5];
+			$user_accueil		= $user_a_modifier[0][6];
+			$user_mailing		= $user_a_modifier[0][7];
+			
+			$checked = $user_mailing == 1 ? "checked" : "";
+		
+			echo "<h2>formulaire de modification de $user_nom</h2><br>";
+			
+			?>
+			
+			<script>
+				// Donne le focus au premier champ du formulaire
+				$('nom').focus();
+			</script>
+			
+			<form action="gestion_utilisateurs/post_utilisateur_personnel.php" method="post" name="post_form" id="post_form">
+				<center>
+				<table width=500>
 				
-				<tr>
-					<TD>Password</TD> 
-					<TD><input type=password name=password value= "<?PHP echo $user_password; ?>"	/></TD>
-				</tr>
-								
-				<tr>
-					<TD>Mail</TD> 
-					<TD><input type=text name=mail value= "<?PHP echo $user_mail; ?>"	/></TD>
-				</tr>
-				
-				<tr>
-					<TD>Mailing</TD> 
-					<TD><input type=checkbox name=mailing <?PHP echo $checked;?>	/></TD>
-				</tr>
-				
-				<tr>
-					<td title="La modification sera visible à la prochaine connexion...">Skin</td>
-					<td>
-						<select name="skin">
-						<?PHP
-							$dossier = opendir("../skins");
-							while ( $skin = readdir($dossier) ) {
-								if ( $skin != "." && $skin != ".." && $skin != ".svn") {
-									$selected = $skin == $user_skin ? "selected" : "" ;
-									echo "<option $selected value=$skin>$skin</option>";
+					<tr>
+						<TD>Nom</TD>
+						<TD><input type=text name=nom id=nom value= "<?PHP echo $user_nom; ?>" 	/></TD>
+					</tr>
+					
+					<tr>
+						<TD>Password</TD> 
+						<TD><input type=password name=password value= "<?PHP echo $user_password; ?>"	/></TD>
+					</tr>
+									
+					<tr>
+						<TD>Mail</TD> 
+						<TD><input type=text name=mail value= "<?PHP echo $user_mail; ?>"	/></TD>
+					</tr>
+					
+					<tr>
+						<TD>Mailing</TD> 
+						<TD><input type=checkbox name=mailing <?PHP echo $checked;?>	/></TD>
+					</tr>
+					
+					<tr>
+						<td title="La modification sera visible à la prochaine connexion...">Skin</td>
+						<td>
+							<select name="skin">
+							<?PHP
+								$dossier = opendir("../skins");
+								while ( $skin = readdir($dossier) ) {
+									if ( $skin != "." && $skin != ".." && $skin != ".svn") {
+										$selected = $skin == $user_skin ? "selected" : "" ;
+										echo "<option $selected value=$skin>$skin</option>";
+									}
 								}
-							}
-							closedir($dossier);
+								closedir($dossier);
+							?>
+							</select>
+						</td>
+					</tr>
+					
+					<tr>
+						<?PHP 
+							$selected = $accueil == $user_accueil ? "selected" : "" ;
 						?>
-						</select>
-					</td>
-				</tr>
+					
+						<TD>Page de Démarrage</TD>
+						<TD><select name="page" size="1">
+								<option value="modules/stats/csschart.php">stats</option>
+								<option value="modules/rss/rss.php">rss</option>
+								<option value="gestion_demandes/voir_demandes.php">demandes</option>
+								<option value="gestion_demandes/voir_interventions.php">interventions</option>
+								<option value="modules/stats/utilisation_parc.php">utilisation parc</option>
+								<option value="modules/wol/voir_liste_wol.php">WOL</option>
+							</select>
+						</TD>
+					</tr>
+					
+				</table>
 				
-				<tr>
-					<?PHP 
-						$selected = $accueil == $user_accueil ? "selected" : "" ;
-					?>
-				
-					<TD>Page de Démarrage</TD>
-					<TD><select name="page" size="1">
-							<option value="modules/stats/csschart.php">stats</option>
-							<option value="modules/rss/rss.php">rss</option>
-							<option value="gestion_demandes/voir_demandes.php">demandes</option>
-							<option value="gestion_demandes/voir_interventions.php">interventions</option>
-							<option value="modules/stats/utilisation_parc.php">utilisation parc</option>
-							<option value="modules/wol/voir_liste_wol.php">WOL</option>
-						</select>
-					</TD>
-				</tr>
-				
-			</table>
-			
-			<br>
-			<input type=submit value='Modifier mon compte' >
+				<br>
+				<input type=submit value='Modifier mon compte' >
 
-			</center>
+				</center>
 
-		</FORM>
+			</FORM>
 		
+		<?PHP
+		}
+		else {
+			
+				echo "<center><h2>Modification du compte $login impossible ! <br> Ce compte de supervision ne doit pas être utilisé en production. <br><br>Merci de créer votre propre compte !</h2></center>";
+			
+		}
+		?>
 		
 <!--	DIV target pour Ajax	-->
 <div id="target"></div>
