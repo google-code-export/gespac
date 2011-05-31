078394d06e27665d4fe6089add1ef4704ff66ca4
eliakim.brahim <eliakim.brahim@98ed3dca-a519-a6af-8faf-6aacfb961b17>
2011-05-31 15:40:53 +0000

    tags 3.3.1 figé

---
+++ tags/3.3.1/gespac/includes.php
@@ -0,0 +1,15 @@
+		<!--	CODAGE	-->
+		<meta http-equiv=Content-Type content="text/html; charset=iso-8859-1" /> 
+		
+<?PHP
+
+	// utiliser pe $_SERVER['HTTP_HOST']; pour avoir la racine du site ...
+
+	include ('../config/databases.php');					// fichiers de configuration des bases de données
+	include ('../fonctions.php');							// fichier contenant les fonctions utilisées dans le reste des scripts
+	include ('../config/pear.php');							// fichiers de configuration des lib PEAR (setinclude + packages)
+	include ('../gestion_authentification/controle.php');	// fichier de vérification de login des utilisateurs
+	
+	
+	header("Content-Type:text/html; charset=iso-8859-15" ); 	// règle le problème d'encodage des caractères
+?>
