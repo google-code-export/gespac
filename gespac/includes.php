5be9b21d00a047df2fae8a235bd0bfabfeb2494c
ati.vallonsdetoulouse <ati.vallonsdetoulouse@98ed3dca-a519-a6af-8faf-6aacfb961b17>
2011-11-18 14:18:49 +0000

    Ajout des include vers la classe SQL

--- gespac/includes.php
+++ gespac/includes.php
@@ -9,6 +9,8 @@
 	include ('../fonctions.php');							// fichier contenant les fonctions utilisées dans le reste des scripts
 	include ('../config/pear.php');							// fichiers de configuration des lib PEAR (setinclude + packages)
 	include ('../gestion_authentification/controle.php');	// fichier de vérification de login des utilisateurs
+	include_once ('../../class/Sql.class.php');				
+	include_once ('../../class/Log.class.php');		
 	
 	
 	header("Content-Type:text/html; charset=iso-8859-15" ); 	// règle le problème d'encodage des caractères
