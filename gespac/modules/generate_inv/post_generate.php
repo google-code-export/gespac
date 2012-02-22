6ce19083ba66291cc94f4bcb9ac228fa61beed08
ati.gastondefferre <ati.gastondefferre@98ed3dca-a519-a6af-8faf-6aacfb961b17>
2012-02-22 14:40:43 +0000

    Introduction de la création des numéros d'inventaires pour les matériels sans plaque id. J'en profite pour corriger une erreur d'index dans le fichier de génération SQL.

---
+++ gespac/modules/generate_inv/post_generate.php
@@ -0,0 +1,48 @@
+<?PHP
+/*
+	// lib
+	require_once ('../../fonctions.php');
+	include_once ('../../config/databases.php');
+	include_once ('../../../class/Sql.class.php');
+
+	$maj_desc 	= $_POST ['import_nom'];
+	$lot 		= $_POST ['pc_a_poster'];
+	$lot_array 	= explode(";", $lot);
+
+	// cnx à la db gespac
+	$con_gespac = new Sql($host, $user, $pass, $gespac);
+	
+	$liste = "";
+
+	// On constitue la requête
+	foreach ($lot_array as $machine) {
+		if ( $machine <> "" ) $liste .= " OR mat_id=$machine";
+	}
+	
+	// rq pour la liste des serial + inventaire
+	$pc_gespac = $con_gespac->QueryAll ("SELECT mat_serial, mat_dsit, mat_nom FROM materiels WHERE mat_id='' $liste");
+	
+	
+	// cnx à la db fog
+	$con_fog = new Sql($host, $user, $pass, $fog);	
+	
+	foreach ($pc_gespac as $pc) {
+			
+		$gespac_serial = $pc['mat_serial'];
+		$gespac_dsit = $pc['mat_dsit'];
+		$gespac_nom = $pc['mat_nom'];
+		
+		// On récupère le hostID grace au serial
+		$hostID = $con_fog->QueryOne ("SELECT iHostID FROM inventory WHERE iSysserial='$gespac_serial'");
+		
+		if ( $maj_desc ) {
+			$con_fog->Execute("UPDATE hosts SET hostName = '$gespac_dsit', hostDesc = '$gespac_nom' WHERE hostID=$hostID");
+		}
+		else {
+			$con_fog->Execute("UPDATE hosts SET hostName = '$gespac_dsit' WHERE hostID=$hostID");
+		}
+
+	}
+*/	
+
+?>
---
+++ gespac/modules/generate_inv/voir_generate.php
@@ -0,0 +1,308 @@
+<?PHP session_start(); ?>
+
+<!--
+	Visualisation des matériels SANS numéro d'inventaire
+	On génère un numéro DSIT UNIQUE avec une codification lourdingue :
+	C pour collège
+	4 derniers chiffres de l'uai
+	1 carac pour le type :
+	 * C pour les pc fixes
+	 * I pour imprimante
+	 * P pour portables
+	 * V pour les tableaux numériques
+	 * E pour écran
+	3 chiffres aléatoires de 000 à 999. En fait on va utiliser l'index du matériel pour s'assurer de son unicité et on bourrera avec des 0.
+	
+
+-->
+
+
+<!--	DIV target pour Ajax	-->
+<div id="target"></div>
+
+
+<?PHP
+
+	// lib
+	require_once ('../../fonctions.php');
+	include_once ('../../config/databases.php');
+	include_once ('../../../class/Sql.class.php');
+
+	
+	// gestion des droits particuliers (Migrer les pc)
+	$droits_supp = ($_SESSION['grade'] == 'root') ? true : preg_match ("#E-07-10#", $_SESSION['droits']);
+	
+
+
+	echo "<h3>CREATION DES NUMEROS d'INVENTAIRE</h3>";
+	echo "<br><small><i>On génère un numéro d'inventaire codifié pour chaque matériel sans numéro DSIT.</small></i>";
+	echo "<br><br>";
+	
+	
+	?>
+
+	<!-- Partie post de la sélection -->
+	<form name="post_form" id="post_form" action="modules/generate_inv/post_generate.php" method="post">
+		<center>
+		<input type=hidden name='pc_a_poster' id='pc_a_poster' value=''>
+		<input type=submit name='post_selection' id='post_selection' value='générer'><br>
+		<span id='nb_selectionnes'> [0] </span> sélectionné(s)	<br>
+		</center>
+		
+	</form>
+	
+	
+	<!-- 	bouton pour le filtrage du tableau	-->
+	<form>
+		<center><small>Filtrer :</small> <input name="filt" onkeyup="filter(this, 'generate_table', '0')" type="text"></center>
+	</form>
+	
+
+	
+	<?PHP
+	
+	// cnx à gespac
+	$con_gespac = new Sql($host, $user, $pass, $gespac);
+	
+	$uai = $con_gespac->QueryOne("SELECT clg_uai FROM college;");
+	
+	// On commence à générer le numéro d'inventaire
+	$inventaire = "C" . substr($uai, 3, 4);
+	
+	
+	// rq pour la liste des PC
+	$liste_materiels_gespac = $con_gespac->QueryAll ("SELECT mat_id, mat_nom, mat_dsit, mat_serial, marque_type, marque_stype FROM materiels, marques WHERE materiels.marque_id=marques.marque_id AND (mat_dsit='' OR mat_dsit IS NULL) ;");
+			
+	/*************************************
+	*
+	*		LISTE DE SELECTION
+	*
+	**************************************/
+
+	echo "<table id='generate_table' width=100%>";
+	
+	$compteur = 0;
+	
+	echo "
+		<th> <input type=checkbox id=checkall onclick=\"checkall('generate_table');\" > </th>
+		<th>id</th>
+		<th>Nom</th>
+		<th>Serial</th>
+		<th>Type</th>
+		<th>Stype</th>
+		<th>Inventaire</th>
+	";
+
+	foreach ($liste_materiels_gespac as $record) {
+		
+		$mat_id	= $record['mat_id'];
+		$nom 	= $record['mat_nom'];
+		$dsit 	= $record['mat_dsit'];
+		$serial	= $record['mat_serial'];
+		$type	= $record['marque_type'];
+		$stype	= $record['marque_stype'];
+		
+		// J'initialise le type à X. comme xorro ;p
+		$id_type = "X";
+		
+		if ( $type == "PC" && $stype == "DESKTOP") $id_type = "C";
+		if ( $type == "PC" && $stype == "PORTABLE") $id_type = "P";
+		if ( $type == "IMPRIMANTE") $id_type = "I";
+		if ( $type == "TBI") $id_type = "V";
+		if ( $type == "ECRAN") $id_type = "E";
+		
+		// bourrage de zero de l'index sur 3 digits
+		$num_unique = sprintf("%1$03d", $mat_id);
+		
+		
+		$numinventaire = $inventaire . $id_type . $num_unique;
+
+				
+		// alternance des couleurs
+		$tr_class = ($compteur % 2) == 0 ? "tr1" : "tr2";
+		
+		echo "<tr id=tr_id$mat_id  class=$tr_class>";
+		
+			echo "<td><input class=chkbx type=checkbox name=chk indexed=true value='$mat_id' onclick=\"select_cette_ligne('$mat_id', $compteur); \"></td>";
+			echo "<td>$mat_id</td>";
+			echo "<td>$nom</td>";
+			echo "<td>$serial</td>";
+			echo "<td>$type</td>";
+			echo "<td>$stype</td>";
+			echo "<td>$numinventaire</td>";
+
+		
+		echo "</tr>";
+	
+		$compteur++;
+		
+	}
+	
+	echo "</table>";	
+
+?>
+
+
+
+
+<script type="text/javascript">
+	
+	
+	window.addEvent('domready', function() {
+
+		// AJAX		
+		$('post_form').addEvent('submit', function(e) {	//	Pour poster un formulaire
+			new Event(e).stop();
+			new Request({
+
+				method: this.method,
+				url: this.action,
+
+				onSuccess: function(responseText, responseXML) {
+					$('target').set('html', responseText);
+					$('conteneur').set('load', {method: 'post'});	//On change la methode d'affichage de la page de GET à POST (en effet, avec GET il récupère la totalité du tableau get en paramètres et lorsqu'on poste la page formation on dépasse la taille maxi d'une url)
+					window.setTimeout("$('conteneur').load('modules/migration_fog/voir_migration.php')", 1500);
+				}
+			
+			}).send(this.toQueryString());
+		}); 
+		
+	
+    });
+	
+	
+	// *********************************************************************************
+	//
+	//				Fonction de filtrage des tables
+	//
+	// *********************************************************************************
+
+	function filter (phrase, _id){
+
+		var words = phrase.value.toLowerCase().split(" ");
+		var table = document.getElementById(_id);
+		var ele;
+		var elements_liste = "";
+				
+		for (var r = 1; r < table.rows.length; r++){
+			
+			ele = table.rows[r].innerHTML.replace(/<[^>]+>/g,"");
+			var displayStyle = 'none';
+			
+			for (var i = 0; i < words.length; i++) {
+				if (ele.toLowerCase().indexOf(words[i])>=0) {	// la phrase de recherche est reconnue
+					displayStyle = '';
+				}	
+				else {	// on masque les rows qui ne correspondent pas
+					displayStyle = 'none';
+					break;
+				}
+			}
+			
+			// Affichage on / off en fonction de displayStyle
+			table.rows[r].style.display = displayStyle;	
+		}
+	}	
+	
+	
+	
+
+	// *********************************************************************************
+	//
+	//				Selection/déselection de toutes les rows
+	//
+	// *********************************************************************************	
+	
+	
+	function checkall(_table) {
+		var table = $(_table);	// le tableau du matériel
+		var checkall_box = $('checkall');	// la checkbox "checkall"
+		
+		for ( var i = 1 ; i < table.rows.length ; i++ ) {
+
+			var lg = table.rows[i].id					// le tr_id (genre tr115)
+			
+			if (checkall_box.checked == true) {
+				document.getElementsByName("chk")[i - 1].checked = true;	// on coche toutes les checkbox
+				select_cette_ligne( lg.substring(5), i, 1 )					//on selectionne la ligne et on ajoute l'index
+			} else {
+				document.getElementsByName("chk")[i - 1].checked = false;	// on décoche toutes les checkbox
+				select_cette_ligne( lg.substring(5), i, 0 )					//on déselectionne la ligne et on la retire de l'index
+			}
+			
+		}
+	}
+	
+	
+	// *********************************************************************************
+	//
+	//				Ajout des index pour postage sur clic de la checkbox
+	//
+	// *********************************************************************************	
+	 
+	function select_cette_ligne( tr_id, num_ligne, check ) {
+
+		var chaine_id = $('pc_a_poster').value;
+		var table_id = chaine_id.split(";");
+
+		var nb_selectionnes = $('nb_selectionnes');
+		
+		var ligne = "tr_id" + tr_id;	//on récupère l'tr_id de la row
+		var li = document.getElementById(ligne);
+		
+		if ( li.style.display == "" ) {	// si une ligne est masquée on ne la selectionne pas (pratique pour le filtre)
+		
+			switch (check) {
+				case 1: // On force la selection si la ligne n'est pas déjà cochée
+					if ( !table_id.contains(tr_id) ) { // la valeur n'existe pas dans la liste
+						table_id.push(tr_id);
+						li.className = "selected";
+						nb_selectionnes.innerHTML = "<small>[" + (table_id.length-1) + "]</small>";	// On entre le nombre de machines sélectionnées	
+					}
+				break;
+				
+				case 0: // On force la déselection
+					if ( table_id.contains(tr_id) ) { // la valeur existe dans la liste on le supprime donc le tr_id de la liste
+						table_id.erase(tr_id);
+						nb_selectionnes.innerHTML = "<small>[" + (table_id.length-1) + "]</small>";	 // On entre le nombre de machines sélectionnées			
+						// alternance des couleurs calculée avec la parité
+						if ( num_ligne % 2 == 0 ) li.className="tr1"; else li.className="tr2";
+					}
+				break;
+				
+				
+				default:	// le check n'est pas précisé, la fonction détermine si la ligne est selectionnée ou pas
+					if ( table_id.contains(tr_id) ) { // la valeur existe dans la liste on le supprime donc le tr_id de la liste
+						table_id.erase(tr_id);
+						
+						nb_selectionnes.innerHTML = "<small>[" + (table_id.length-1) + "]</small>";	 // On entre le nombre de machines sélectionnées			
+
+						// alternance des couleurs calculée avec la parité
+						if ( num_ligne % 2 == 0 ) li.className="tr1"; else li.className="tr2";
+					
+					} else {	// le tr_id n'est pas trouvé dans la liste, on créé un nouvel tr_id à la fin du tableau
+						table_id.push(tr_id);
+						li.className = "selected";
+						nb_selectionnes.innerHTML = "<small>[" + (table_id.length-1) + "]</small>";	// On entre le nombre de machines sélectionnées	
+					}
+				break;			
+			}
+	
+			// on concatène tout le tableau dans une chaine de valeurs séparées par des ;
+			$('pc_a_poster').value = table_id.join(";");
+			
+			if ( $('pc_a_poster').value != "" ) {
+				$('post_selection').style.display = "";
+
+			} else { 
+				$('post_selection').style.display = "none";
+			}
+
+		}
+	}
+	
+	
+	
+	
+</script>
+
