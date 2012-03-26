9fa93379c352df2c11df827e5f995cac913bdb27
ati.gastondefferre <ati.gastondefferre@98ed3dca-a519-a6af-8faf-6aacfb961b17>
2012-03-26 08:24:54 +0000

    changement du codage de utf8 en iso-8859-1

--- index.php
+++ index.php
@@ -19,9 +19,9 @@
 		<!--	CSS	-->
 		<link rel="stylesheet" href="./gespac/css/style_ff.css" type="text/css" media="screen" />
 		
-		<!--Script de dÃ©tection pour savoir si il y a un popup killer-->
+		<!--Script de détection pour savoir si il y a un popup killer-->
 		<script type="text/JavaScript" language="javascript">
-Â 			var mine = window.open('','','width=1,height=1,left=0,top=0,scrollbars=no');
+			var mine = window.open('','','width=1,height=1,left=0,top=0,scrollbars=no');
 			if(mine)
 				var popUpsBlocked = false
 			else
@@ -60,13 +60,13 @@
 	include ('version');
 	
 	
-		// on vÃ©rifie la connectivitÃ© avec le serveur avant d'aller plus loin
+		// on vérifie la connectivité avec le serveur avant d'aller plus loin
 		if(!mysql_connect($host, $user, $pass)) {
 			echo 'Merci de renseigner le fichier "config.php" se trouvant dans le dossier include.<br>';
 			exit();
 		}
 
-		// on vÃ©rifie la connectivitÃ© avec la base avant d'aller plus loin	
+		// on vérifie la connectivité avec la base avant d'aller plus loin	
 		if(!mysql_select_db($gespac)) {
 			echo '<img src="./gespac/img/info.png"><br>
 			vous devez installer au prÃ©alable la base de donnÃ©es en cliquant <a href="install_mysql/installation.php">ici</a>';
@@ -77,34 +77,34 @@
 		
 		session_start();
 		
-		// on vÃ©rifie si l'utilisateur est identifiÃ©
+		// on vérifie si l'utilisateur est identifié
 		if (!isset( $_SESSION['login'])) {
-			// la variable de session n'existe pas, donc l'utilisateur n'est pas authentifiÃ© -> On redirige sur la page permettant de s'authentifier
+			// la variable de session n'existe pas, donc l'utilisateur n'est pas authentifié -> On redirige sur la page permettant de s'authentifier
 			echo '<img src="./gespac/img/gespac.png" height=48> '.$version;
 			include 'login.php';
-			exit();	// on arrÃªte l'exÃ©cution
+			exit();	// on arrête l'exécution
 
 		} else {
 
 			//$display_icon = ( $_SESSION['grade'] < 2 ) ? "" : "none" ;
 
-			// si le grade du compte est root, on donne automatiquement les droits d'accÃ¨s aux icones. Sinon, on teste si le compte a accÃ¨s aux icones sinon.
+			// si le grade du compte est root, on donne automatiquement les droits d'accés aux icones. Sinon, on teste si le compte a accés aux icones sinon.
 			
 				
 			echo "<div class=portail-menu-item><a href='./gespac'> 
 				<img src='./gespac/img/gespac.png' height=48><br>GESPAC </a></div>";
 			
-			// On rÃ©cupÃ¨re adresse du serveur pour le menu DELL https port 1311 et webmin sur port 10000
+			// On récupère adresse du serveur pour le menu DELL https port 1311 et webmin sur port 10000
 			$adresse = $_SERVER['SERVER_ADDR'];
 
 	
 			// adresse de connexion Ã  la base de donnÃ©es
 			$dsn_gespac     = 'mysql://'. $user .':' . $pass . '@localhost/' . $gespac;
 
-			// cnx Ã  la base de donnÃ©es GESPAC
+			// cnx Ã  la base de données GESPAC
 			$db_gespac 	= & MDB2::factory($dsn_gespac);
 
-			// stockage des lignes retournÃ©es par sql dans un tableau nommÃ© liste_des_materiels
+			// stockage des lignes retournées par sql dans un tableau nommÃ© liste_des_materiels
 			$liste_des_icones = $db_gespac->queryAll ( "SELECT mp_id, mp_nom, mp_url, mp_icone FROM menu_portail ORDER BY mp_nom" );	
 			
 				
@@ -120,12 +120,8 @@
 
 				//On change l'adresse de l'url pour l'application dell et webmin qui utilise du https avec des ports on se base sur le nom du menu portail
 				if ($mp_nom == 'dell') {$mp_url = 'https://'.$adresse.':1311';} elseif ($mp_nom == 'webmin') {$mp_url = 'https://'.$adresse.':10000';}
+
 				//On reprend l'affichage du menu
-
-//en double		//if ( $affiche_item )
-					//echo "<div class=portail-menu-item><a href='$mp_url' target=_blank> <img src='./gespac/img/$mp_icone' height=48><br>$mp_nom</a> </div>";
-
-//fin du en double
 				if ( $affiche_item ) {
 					if ( file_exists("./gespac/img/$mp_icone") ) $icon_path = "./gespac/img/$mp_icone";
 					else $icon_path = "./gespac/img/application.png";
@@ -136,7 +132,7 @@
 			}	
 			
 			echo "<div style='float:right;' class=portail-menu-item><a href='./gespac/gestion_authentification/logout.php'> 
-				<img src='./gespac/img/cancel.png' height=48><br>DÃ©connexion </a></div>";
+				<img src='./gespac/img/cancel.png' height=48><br>Déconnexion </a></div>";
 
 			echo "<div class='spacer'> </div>";
 	
@@ -144,9 +140,9 @@
 		}
 	?>
 	
-	<!--On lance la dÃ©tection du popup killer -->
+	<!--On lance la détection du popup killer -->
 	<script type="text/JavaScript" language="JavaScript">
-		if ( popUpsBlocked ) alert('POPUP KILLERS :\nPrÃªter ou rendre un portable gÃ©nÃ¨re un popup pour les conventions.\nLe popup killer bloquera l`affichage de ces conventions.\nPensez Ã  autoriser les popups pour GESPAC.');
+		if ( popUpsBlocked ) alert('POPUP KILLERS :\nPrêter ou rendre un portable génère un popup pour les conventions.\nLe popup killer bloquera l`affichage de ces conventions.\nPensez à autoriser les popups pour GESPAC.');
 	</script>
 	
 	</h3>
@@ -158,12 +154,12 @@
 		
 		
       <br/><br/>
-		<b><h3><center>GESPAC est rÃ©gi par la licence CeCILL V2 soumise au droit franÃ§ais et respectant les principes de diffusion des logiciels libres.<br>
-								Vous pouvez utiliser, modifier et/ou redistribuer ce programme sous les conditions de la licence CeCILL telle que diffusÃ©e par le CEA, le CNRS et l'INRIA  sur le site "http://www.cecill.info".</b></h3></center><br><br>
+		<b><h3><center>GESPAC est régi par la licence CeCILL V2 soumise au droit français et respectant les principes de diffusion des logiciels libres.<br>
+								Vous pouvez utiliser, modifier et/ou redistribuer ce programme sous les conditions de la licence CeCILL telle que diffusée par le CEA, le CNRS et l'INRIA  sur le site "http://www.cecill.info".</b></h3></center><br><br>
 				<b>SITE OFFICIEL : </b><br/>
-					<a href="http://gespac.free.fr" target=_blank>GESPAC</a> (Les procÃ©dures et manuels validÃ©s)<br/><br/>
+					<a href="http://gespac.free.fr" target=_blank>GESPAC</a> (Les procédures et manuels validés)<br/><br/>
 		<b>NAVIGATEURS : </b><br/>
-			- Gespac marche mieux avec les navigateurs respectant la norme W3C (globalement si le navigateur gÃ¨re le css3, pas de problÃ¨me)<br/>
+			- Gespac marche mieux avec les navigateurs respectant la norme W3C (globalement si le navigateur gère le css3, pas de problème)<br/>
         - Dans le cas ou vous utilisez Internet Explorer, Gespac tentera d'installer <a href="http://code.google.com/intl/fr-FR/chrome/chromeframe/" target=_blank>Google Frame</a><br/>
 	</div>
 
