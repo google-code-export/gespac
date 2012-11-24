b32a6c993ba04546d068c1cc98f5941633634dbb
ati.gastondefferre@gmail.com <ati.gastondefferre@gmail.com@98ed3dca-a519-a6af-8faf-6aacfb961b17>
2012-11-24 11:23:56 +0000

    Suppression du dossier GESTION_AUTHENTIFICATION. Ce dossier contenait des fichiers obsolètes (et parfois sans référence).
    Le fichier logout.php est désormais à la racine (à côté du login, ce qui est plus logique)

--- branches/3.4/gespac/includes.php
+++ branches/3.4/gespac/includes.php
@@ -7,10 +7,11 @@
 
 	include ('../config/databases.php');					// fichiers de configuration des bases de données
 	include ('../fonctions.php');							// fichier contenant les fonctions utilisées dans le reste des scripts
-	include ('../config/pear.php');							// fichiers de configuration des lib PEAR (setinclude + packages)
-	include ('../gestion_authentification/controle.php');	// fichier de vérification de login des utilisateurs
-	include_once ('../../class/Sql.class.php');				
+	//include ('../config/pear.php');							// fichiers de configuration des lib PEAR (setinclude + packages)
+	//include ('../gestion_authentification/controle.php');	// fichier de vérification de login des utilisateurs
 	include_once ('../../class/Log.class.php');		
+	include_once ('../../class/Sql.class.php');				
+	
 	
 	
 	header("Content-Type:text/html; charset=iso-8859-15" ); 	// règle le problème d'encodage des caractères
