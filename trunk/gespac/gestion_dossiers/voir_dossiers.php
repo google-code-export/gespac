0fbd7fd4f80f4151abd271f8059b9fc8082ff7f1
ati.gastondefferre@gmail.com <ati.gastondefferre@gmail.com@98ed3dca-a519-a6af-8faf-6aacfb961b17>
2012-12-16 20:12:50 +0000

    option pour montrer/masquer les dossiers clos

--- trunk/gespac/gestion_dossiers/voir_dossiers.php
+++ trunk/gespac/gestion_dossiers/voir_dossiers.php
@@ -15,6 +15,8 @@
 	$E_chk = ($_SESSION['grade'] == 'root') ? true : preg_match ("#E-03-03#", $_SESSION['droits']);
 		
 	
+	$filtre = $_GET["filter"];	
+	$showclos = $_GET["showclos"];	
 ?>
 
 
@@ -26,6 +28,11 @@
 	<span class="entetes-options">
 		
 		<span class="option"><?PHP if ( $E_chk ) echo "<a href='gestion_dossiers/form_dossiers.php?height=750&width=750&id=-1' rel='slb_dossiers' title='Ajouter un dossier'> <img src='img/icons/add.png'></a>";?></span>
+		
+		<span class="option"><?PHP 
+			if (!$showclos) echo "<a href='index.php?page=dossiers&showclos=1' title='Montrer les dossiers clos'> <img src='img/icons/eye.png'></a>";
+			else echo "<a href='index.php?page=dossiers' title='Cacher les dossiers clos'> <img src='img/icons/eye.png'></a>";?></span>
+			
 		<span class="option">
 			<!-- 	bouton pour le filtrage du tableau	-->
 			<form id="filterform"> <input placeholder=" filtrer" name="filt" id="filt" onKeyPress="return disableEnterKey(event)" onkeyup="filter(this, 'dossiers_table');" type="text" value=<?PHP echo $_GET['filter'];?>> </form>
@@ -38,9 +45,7 @@
 
 
 <?PHP 
-	$filtre = $_GET["filter"];	
-		
-	$liste_dossiers = $con_gespac->QueryAll ("SELECT dossiers.dossier_id as dossier_id, dossier_type, dossier_mat, txt_date, txt_etat, txt_texte FROM dossiers, dossiers_textes WHERE dossiers.dossier_id = dossiers_textes.dossier_id GROUP BY dossiers.dossier_id ORDER BY dossier_id DESC;");
+	$liste_dossiers = $con_gespac->QueryAll ("SELECT dossiers.dossier_id as dossier_id, dossier_type, dossier_mat, txt_date, txt_etat, txt_texte FROM dossiers, dossiers_textes WHERE dossiers.dossier_id=dossiers_textes.dossier_id GROUP BY dossiers.dossier_id ORDER BY dossier_id DESC;");
 		
 	echo "<table id='dossiers_table'>";
 	
@@ -66,68 +71,73 @@
 			
 			$last_etat		= $con_gespac->QueryOne("SELECT txt_etat FROM dossiers_textes WHERE dossier_id=$dossier_id ORDER BY txt_date DESC");
 			
-			// alternance des couleurs
-			$tr_class = ($compteur % 2) == 0 ? "tr1" : "tr2";
-			
-			// Le dossier
-			
-			echo "<tr id='tr_$dossier_id' class=$tr_class>";
-				echo "<td width=20px><a href='#' onclick=\"toggleDossier($dossier_id);\"><img src='img/deplier.png'></a></td>";
-				echo "<td width=20px>$dossier_id</td>";
-				echo "<td width=100px>$date_ouverture</td>";
-				echo "<td width=60px>$dossier_type</td>";
-				echo "<td width=60px class='td_$last_etat'>$last_etat</td>";
-				echo "<td>$txt_texte</td>";
-				echo "<td width=20px><a href='gestion_dossiers/liste_materiels.php?height=480&width=500&dossier=$dossier_id' rel='slb_dossiers' title='Liste des matériels du dossier $dossier_id'><img src='img/outils.png'></a></td>";
-				if ( $E_chk && $last_etat<>"clos") echo "<td width=20px><a href='#' onclick=\"AffichePage('conteneur', 'gestion_dossiers/form_dossiers.php?id=$dossier_id');\"> <img src='img/write.png'> </a></td>";
-				else echo "<td>&nbsp;</td>";
-				if ( $E_chk && $last_etat<>"clos") echo "<td width=20px><a href='#' onclick=\"validation_suppr_dossier('$dossier_id');\"> <img src='img/delete.png'> </a></td>";
-				else echo "<td>&nbsp;</td>";
-			
-			echo "</tr>";
-			
-			
-			
-			// 	Les pages du dossier 	 	 	 	 
-			$page_dossier = $con_gespac->QueryAll ("SELECT txt_id, txt_date, txt_texte, txt_etat, users.user_nom FROM dossiers_textes, users WHERE dossier_id=$dossier_id AND txt_user=user_id");
-
-			echo "<tr id='dossiers$dossier_id' style='display:none' class='inner_tr'>";
-			
-			echo "<td colspan=9 class='inner_td'>";
-				echo "<table class='innertable'>";
-
-					foreach ( $page_dossier as $page) {
-						
-						$txt_id 	= $page['txt_id'];
-						$txt_date 	= date ("d-m-Y H:i", strtotime($page['txt_date']));
-						$txt_texte 	= stripcslashes($page['txt_texte']);
-						$txt_etat 	= $page['txt_etat'];
-						if (strtoupper($_SESSION['grade']) == 'ATI' || strtoupper($_SESSION['grade']) == 'ROOT') {
-							$user_nom 	= $page['user_nom'];
-						} else {
-							$user_nom 	= 'Anonyme';
-						} 
-						
-						
-						echo "<tr>";
-							echo "<td class='td_$txt_etat'>$txt_etat</td>";
-							echo "<td class='td_$txt_etat'>$txt_date</td>";
-							echo "<td class='td_$txt_etat'>$user_nom</td>";
-						echo "</tr>";
-						
-						echo "<tr>";
-							echo "<td colspan=4>$txt_texte</td>";
-						echo "</tr>";
-						
-						//echo "<tr><td colspan=4 style='border:none;background-color:white;'>&nbsp;</td></tr>";
-						
-					}
-					
+			// Voir aussi les dossiers clos
+			if ( $showclos || $last_etat <> "clos" ) {
+							
+				// alternance des couleurs
+				$tr_class = ($compteur % 2) == 0 ? "tr1" : "tr2";
 				
-				echo "</table>";
-			echo "</td></tr>";
+				// Le dossier
+				
+				echo "<tr id='tr_$dossier_id' class=$tr_class>";
+					echo "<td width=20px><a href='#' onclick=\"toggleDossier($dossier_id);\"><img src='img/deplier.png'></a></td>";
+					echo "<td width=20px>$dossier_id</td>";
+					echo "<td width=100px>$date_ouverture</td>";
+					echo "<td width=60px>$dossier_type</td>";
+					echo "<td width=60px class='td_$last_etat'>$last_etat</td>";
+					echo "<td>$txt_texte</td>";
+					echo "<td width=20px><a href='gestion_dossiers/liste_materiels.php?height=480&width=500&dossier=$dossier_id' rel='slb_dossiers' title='Liste des matériels du dossier $dossier_id'><img src='img/outils.png'></a></td>";
+					if ( $E_chk && $last_etat<>"clos") echo "<td width=20px><a href='#' onclick=\"AffichePage('conteneur', 'gestion_dossiers/form_dossiers.php?id=$dossier_id');\"> <img src='img/write.png'> </a></td>";
+					else echo "<td>&nbsp;</td>";
+					if ( $E_chk && $last_etat<>"clos") echo "<td width=20px><a href='#' onclick=\"validation_suppr_dossier('$dossier_id');\"> <img src='img/delete.png'> </a></td>";
+					else echo "<td>&nbsp;</td>";
+				
+				echo "</tr>";
+				
+				
+				
+				// 	Les pages du dossier 	 	 	 	 
+				$page_dossier = $con_gespac->QueryAll ("SELECT txt_id, txt_date, txt_texte, txt_etat, users.user_nom FROM dossiers_textes, users WHERE dossier_id=$dossier_id AND txt_user=user_id");
 
-			$compteur++;
+				echo "<tr id='dossiers$dossier_id' style='display:none' class='inner_tr'>";
+				
+				echo "<td colspan=9 class='inner_td'>";
+					echo "<table class='innertable'>";
+
+						foreach ( $page_dossier as $page) {
+							
+							$txt_id 	= $page['txt_id'];
+							$txt_date 	= date ("d-m-Y H:i", strtotime($page['txt_date']));
+							$txt_texte 	= stripcslashes($page['txt_texte']);
+							$txt_etat 	= $page['txt_etat'];
+							if (strtoupper($_SESSION['grade']) == 'ATI' || strtoupper($_SESSION['grade']) == 'ROOT') {
+								$user_nom 	= $page['user_nom'];
+							} else {
+								$user_nom 	= 'Anonyme';
+							} 
+							
+							
+							echo "<tr>";
+								echo "<td class='td_$txt_etat'>$txt_etat</td>";
+								echo "<td class='td_$txt_etat'>$txt_date</td>";
+								echo "<td class='td_$txt_etat'>$user_nom</td>";
+							echo "</tr>";
+							
+							echo "<tr>";
+								echo "<td colspan=4>$txt_texte</td>";
+							echo "</tr>";
+							
+							//echo "<tr><td colspan=4 style='border:none;background-color:white;'>&nbsp;</td></tr>";
+							
+						}
+						
+					
+					echo "</table>";
+				echo "</td></tr>";
+				
+				$compteur++;
+			}
+			
 
 		}
 		
