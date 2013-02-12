977eada94d77a8df615308e45c051872f9f3a797
ati.gastondefferre@gmail.com <ati.gastondefferre@gmail.com@98ed3dca-a519-a6af-8faf-6aacfb961b17>
2013-02-12 07:07:54 +0000

    Migration de la partie collège (form+post+voir)

--- gespac/gestion_college/post_college.php
+++ gespac/gestion_college/post_college.php
@@ -4,25 +4,27 @@
 	/* fichier de creation / modif / du college
 	
 	Si j'ai un ID c'est une modification
-	Si j'en ai pas c'est une création
+	Si j'en ai pas c'est une crÃ©ation
 	
-	reste à coder pour la suppression
+	reste Ã  coder pour la suppression
 	
 	*/
 	
-	header("Content-Type:text/html; charset=iso-8859-1" ); 	// règle le problème d'encodage des caractères
-
 	// lib
-	include ('../includes.php');	// fichier contenant les fonctions, la config pear, les mdp databases ...
-	
-	// cnx à la base de données GESPAC
+	require_once ('../fonctions.php');
+	include_once ('../config/databases.php');
+	include_once ('../../class/Log.class.php');		
+	include_once ('../../class/Sql.class.php');	
+
+
+	// cnx Ã  la base de donnÃ©es GESPAC
 	$con_gespac = new Sql($host, $user, $pass, $gespac);
 	
-	//Log des requêtes SQL
+	//Log des requÃªtes SQL
 	$log = new Log ("../dump/log_sql.sql");
 	
 		
-	// on récupère les paramètres de l'url	
+	// on rÃ©cupÃ¨re les paramÃ¨tres de l'url	
 	$action 	= $_GET['action'];
 	
 
@@ -41,7 +43,6 @@
 	
 	if ( $action == 'creat' ) {
 		
-		//la fonction addslashes() va permettre de rajouter un slash avant une apostrophe dans un string. Pas besoin d'utiliser cette fonction pour les champs uniquement numériques comme le téléphone, fax...
 		$clg_uai 		= $_POST['clg_uai'];
 		$clg_nom 		= strtoupper(addslashes(utf8_decode(urldecode($_POST['clg_nom']))));
 		$clg_ati 		= strtoupper(addslashes(utf8_decode(urldecode($_POST['clg_ati']))));
@@ -54,57 +55,57 @@
 		$clg_web 		= addslashes(utf8_decode(urldecode($_POST['clg_web'])));
 		$clg_grr 		= addslashes(utf8_decode(urldecode($_POST['clg_grr'])));
 	
-		// création du collège
+		// crÃ©ation du collÃ¨ge
 		$req_add_college = "INSERT INTO college VALUES ( '$clg_uai', '$clg_nom', '$clg_ati', '$clg_ati_mail', '$clg_adresse', '$clg_cp', '$clg_ville', '$clg_tel', '$clg_fax', '$clg_web', '$clg_grr')";
 		$con_gespac->Execute ( $req_add_college );
 		
-		// On log la requête SQL
+		// On log la requÃªte SQL
 		$log->Insert( $req_add_college );
 		
-		echo "<br><small>Création du collège <b>$clg_nom</b> !</small><br>";
+		echo "CrÃ©ation du collÃ¨ge <b>$clg_nom</b> !";
 		
-		// Mise à jour de l'adresse mail dans le compte ati
+		// Mise Ã  jour de l'adresse mail dans le compte ati
 		$req_maj_mail_ati = "UPDATE users SET user_mail = '$clg_ati_mail' WHERE user_nom='ati'";
 		$con_gespac->Execute ( $req_maj_mail_ati );
 		
-		// On log la requête SQL
+		// On log la requÃªte SQL
 		$log->Insert( $req_maj_mail_ati );
 		
-		echo "<br><small>Mise à jour du mail du compte ATI !</small><br>";
+		echo "<br>Mise Ã  jour du mail du compte ATI !";
 		
-		// Création de diverses salles
+		// CrÃ©ation de diverses salles
 		$req_add_salle_stock = "INSERT INTO salles (salle_id, salle_nom, clg_uai, est_modifiable) VALUES (1, 'STOCK', '$clg_uai', 0 )";
 		$con_gespac->Execute ( $req_add_salle_stock );
 		
-		// On log la requête SQL
+		// On log la requÃªte SQL
 		$log->Insert( $req_add_salle_stock );
 		
-		echo "<small>Création de la salle <b>Stock</b> !</small><br>";
+		echo "<br>CrÃ©ation de la salle <b>Stock</b> !";
 		
 		$req_add_salle_d3e = "INSERT INTO salles (salle_id, salle_nom, clg_uai, est_modifiable) VALUES (2, 'D3E', '$clg_uai', 0 )";
 		$con_gespac->Execute ( $req_add_salle_d3e );
 		
-		// On log la requête SQL
+		// On log la requÃªte SQL
 		$log->Insert( $req_add_salle_d3e );
 		
-		echo "<small>Création de la salle <b>D3E</b> !</small><br>";
+		echo "<br>CrÃ©ation de la salle <b>D3E</b> !";
 		
 		$req_add_salle_pret = "INSERT INTO salles (salle_id, salle_nom, clg_uai, est_modifiable) VALUES (3, 'PRETS', '$clg_uai', 0 )";
 		$con_gespac->Execute ( $req_add_salle_pret );
 		
-		// On log la requête SQL
+		// On log la requÃªte SQL
 		$log->Insert( $req_add_salle_pret );
 		
-		echo "<small>Création de la salle <b>PRETS</b> !</small><br>";
+		echo "<br>CrÃ©ation de la salle <b>PRETS</b> !";
 
 		//Insertion d'un log
 		
-		$log_texte = "La fiche informative a été créée. Nom actuel : <b>$clg_nom</b>";
+		$log_texte = "La fiche informative a Ã©tÃ© crÃ©Ã©e. Nom actuel : <b>$clg_nom</b>";
 		
-		$req_log_creer_college = "INSERT INTO logs ( log_type, log_texte ) VALUES ( 'Création collège', '$log_texte');";
+		$req_log_creer_college = "INSERT INTO logs ( log_type, log_texte ) VALUES ( 'CrÃ©ation collÃ¨ge', '$log_texte');";
 		$con_gespac->Execute ( $req_log_creer_college );
 		
-		// On log la requête SQL
+		// On log la requÃªte SQL
 		$log->Insert( $req_log_creer_college );
 		
 	}
@@ -116,7 +117,7 @@
 	
 	if ( $action == 'mod' ) {
 		
-		//la fonction addslashes() va permettre de rajouter un slash avant une apostrophe dans un string. Pas besoin d'utiliser cette fonction pour les champs uniquement numériques comme le téléphone, fax...
+		//la fonction addslashes() va permettre de rajouter un slash avant une apostrophe dans un string. Pas besoin d'utiliser cette fonction pour les champs uniquement numÃ©riques comme le tÃ©lÃ©phone, fax...
 		$old_uai		= $_POST['old_uai'];
 		$clg_uai 		= $_POST['clg_uai'];
 		$clg_nom 		= strtoupper(addslashes(utf8_decode(urldecode($_POST['clg_nom']))));
@@ -135,32 +136,30 @@
 							  clg_site_web = '$clg_web', clg_site_grr = '$clg_grr' WHERE clg_uai='$old_uai'";
 		$con_gespac->Execute ( $req_modif_college );
 		
-		// On log la requête SQL
+		// On log la requÃªte SQL
 		$log->Insert( $req_modif_college );
 		
-		// Mise à jour de l'adresse mail dans le compte ati
+		// Mise Ã  jour de l'adresse mail dans le compte ati
 		$req_maj_mail_ati = "UPDATE users SET user_mail = '$clg_ati_mail' WHERE user_nom='ati'";
 		$con_gespac->Execute ( $req_maj_mail_ati );
 		
-		// On log la requête SQL
+		// On log la requÃªte SQL
 		$log->Insert( $req_maj_mail_ati );
-		
-		echo "<br><small>Mise à jour du mail du compte ATI !</small><br>";
 		
 		
 		//Insertion d'un log
 		
-		$log_texte = "La fiche informative a été modifiée. Nom actuel : <b>$clg_nom</b>";
+		$log_texte = "La fiche informative a Ã©tÃ© modifiÃ©e. Nom actuel : <b>$clg_nom</b>";
 		
-		$req_log_modif_college = "INSERT INTO logs ( log_type, log_texte ) VALUES ( 'Modification collège', '$log_texte');";
+		$req_log_modif_college = "INSERT INTO logs ( log_type, log_texte ) VALUES ( 'Modification collÃ¨ge', '$log_texte');";
 		$con_gespac->Execute ( $req_log_modif_college );
 		
-		// On log la requête SQL
+		// On log la requÃªte SQL
 		$log->Insert( $req_log_modif_college );
 		
 		$clg_nom = stripslashes($clg_nom);
 		
-		echo "<small>Modification du collège <b>$clg_nom</b>.</small>";
+		echo "Modification du collÃ¨ge <b>$clg_nom</b>";
 	
 	}
 	
--- gespac/gestion_college/voir_college.php
+++ gespac/gestion_college/voir_college.php
@@ -1,38 +1,17 @@
 <?PHP
 session_start();
 
-	/* 
-		fichier de creation / modif / du college
-
-	*/
-
-
-?>
-
-<!--	DIV target pour Ajax	-->
-<div id="target"></div>
-
-<script type="text/javascript">	
-	// init de la couleur de fond
-	$('conteneur').style.backgroundColor = "#fff";
-	
-</script>
+	#fichier de creation / modif / du college
 
 	
-<?PHP
-
-	header("Content-Type:text/html; charset=iso-8859-1" ); 	// règle le problème d'encodage des caractères
-	
-	include ('../includes.php');	// fichier contenant les fonctions, la config pear, les mdp databases ...
-
-	// si le grade du compte est root, on donne automatiquement les droits d'accès en écriture. Sinon, on teste si le compte a accès à la page.
+	// si le grade du compte est root, on donne automatiquement les droits d'accÃ¨s en Ã©criture. Sinon, on teste si le compte a accÃ¨s Ã  la page.
 	$E_chk = ($_SESSION['grade'] == 'root') ? true : preg_match ("#E-08-01#", $_SESSION['droits']);
-	
-	
-	// cnx à la base de données GESPAC
+
+
+	// cnx Ã  la base de donnÃ©es GESPAC
 	$con_gespac 	= new Sql ($host, $user, $pass, $gespac);
 	
-	// stockage des lignes retournées par sql dans un tableau nommé avec originalité "array" (mais "tableau" peut aussi marcher)
+	// stockage des lignes retournÃ©es par sql dans un tableau nommÃ© avec originalitÃ© "array" (mais "tableau" peut aussi marcher)
 	$college_info = $con_gespac->QueryRow ( "SELECT clg_uai, clg_nom, clg_ati, clg_ati_mail, clg_adresse, clg_cp, clg_ville, clg_tel, clg_fax, clg_site_web, clg_site_grr FROM college;" );
 
 	$clg_uai 		= stripslashes($college_info [0]);
@@ -46,11 +25,29 @@ session_start();
 	$clg_fax 		= $college_info [8];
 	$clg_site_web 	= stripslashes($college_info [9]);
 	$clg_site_grr 	= stripslashes($college_info [10]);
+
+?>
+
+
+<div class="entetes" id="entete-college">	
+
+	<span class="entetes-titre">FICHE COLLEGE<img class="help-button" src="img/icons/info.png"></span>
+	<div class="helpbox">C'est la fiche d'identitÃ© du collÃ¨ge.</div>
+
+	<span class="entetes-options">
+		<span class="option"><?PHP if ( $E_chk ) echo "<a href='gestion_college/form_college.php?height=450&width=640&id=$clg_uai' rel='slb_college' title=\"Modifier la fiche collÃ¨ge\"><img src='img/icons/modif1.png'></a>";?></span>
+	</span>
+
+</div>
+
+<div class="spacer"></div>
+
 	
-	
-echo "<h3>Fiche d'informations du collège $clg_nom</h3><br>
+<?PHP
+		
+echo "
 	<center>
-			<table class='tablehover' width=450>
+			<table class='tablehover'>
 
 				<tr class='tr1'>
 					<TD><B>UAI</B></TD>
@@ -58,7 +55,7 @@ echo "<h3>Fiche d'informations du collège $clg_nom</h3><br>
 				</tr>
 			
 				<tr class='tr2'>
-					<TD><B>Nom du collège</B></TD>
+					<TD><B>Nom du collÃ¨ge</B></TD>
 					<TD>$clg_nom</TD>
 				</tr>
 			
@@ -73,7 +70,7 @@ echo "<h3>Fiche d'informations du collège $clg_nom</h3><br>
 				</tr>
 			
 				<tr class='tr1'>
-					<TD><B>Adresse du collège</B></TD>
+					<TD><B>Adresse du collÃ¨ge</B></TD>
 					<TD>$clg_adresse</TD>
 				</tr>
 			
@@ -88,7 +85,7 @@ echo "<h3>Fiche d'informations du collège $clg_nom</h3><br>
 				</tr>
 				
 				<tr class='tr2'>
-					<TD><B>Téléphone</B></TD>
+					<TD><B>TÃ©lÃ©phone</B></TD>
 					<TD>$clg_tel</TD>
 				</tr>
 			
@@ -98,19 +95,24 @@ echo "<h3>Fiche d'informations du collège $clg_nom</h3><br>
 				</tr>
 			
 				<tr class='tr2'>
-					<TD><B>Site web du collège</B></TD>
+					<TD><B>Site web du collÃ¨ge</B></TD>
 					<TD><a href='http://$clg_site_web' target=_blank>http://$clg_site_web</a></TD>
 				</tr>				
 			
 				<tr class='tr1'>
-					<TD><B>Accès GRR</B></TD>
+					<TD><B>AccÃ¨s GRR</B></TD>
 					<TD><a href='http://$clg_site_grr' target=_blank>http://$clg_site_grr</a></TD>
 				</tr>	
 				
 			</table>
 		</center>";
 			
-if ($E_chk)	
-	echo "<br><center><a href='#&id=$clg_uai' onclick=\"$('conteneur').load('gestion_college/form_college.php?id=$clg_uai');\" ><img src='img/write.png' title='Modifier les informations du collège'></a></center>";
-
 ?>
+
+
+<script type="text/javascript">
+	
+	window.addEvent('domready', function(){
+		SexyLightbox = new SexyLightBox({color:'black', dir: 'img/sexyimages', find:'slb_college'});
+	});
+</script>
