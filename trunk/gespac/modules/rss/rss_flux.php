ccb3134f47ddaf732a06763b6826ed80a5e403ac
ati.gastondefferre@gmail.com <ati.gastondefferre@gmail.com@98ed3dca-a519-a6af-8faf-6aacfb961b17>
2013-03-29 13:46:43 +0000

    Migration et correction du module flux RSS

--- trunk/gespac/modules/rss/rss_flux.php
+++ trunk/gespac/modules/rss/rss_flux.php
@@ -1,40 +1,41 @@
 <?PHP
-	require_once("commonlib.php");
-	
-	header("Content-Type:text/html; charset=iso-8859-1" ); 	// règle le problème d'encodage des caractères
-	
-	
-	$ligne = $_GET['page'];
-	
-		$handle = fopen("../../dump/flux.txt", "r");
 
-		$row = 0;
+	require_once ("commonlib.php");
+
+	$ligne = $_GET['flux'];
+	
+	if (!$ligne) $ligne=0;
 		
-		while (($data = fgetcsv($handle, 1000, ";")) !== FALSE) {
-			$line[$row][0] = $data[0];	
-			$line[$row][1] = $data[1];	
-			
-			$row++;
-		}
+	$handle = fopen("dump/flux.txt", "r");
 
-		$url = $line[$ligne][1];
+	$row = 0;
+		
+	while (($data = fgetcsv($handle, 1000, ";")) !== FALSE) {
+		$line[$row][0] = $data[0];	
+		$line[$row][1] = $data[1];	
+		
+		$row++;
+	}
+
+	$url = $line[$ligne][1];
 	
-		fclose ($handle);
-
+	echo "<small><b>" . $line[$ligne][0] . "</b> sur " . $url . "</small>";
+	
+	
+	fclose ($handle);
 		
 	$page = file_get_contents($url, false);
 
-	
 	if ($page == true) {
 		// On stocke le fichier en local
-		$handle = fopen("../../dump/flux_courant.xml", "w+");	
+		$handle = fopen("dump/flux_courant.xml", "w+");	
 		fwrite($handle, $page);
 		fclose ($handle);
 		
-		// On affiche le flux mais converti en Latin
-		echo $output = iconv( "UTF-8", "CP1252", Common_Display("../../dump/flux_courant.xml", 25, true, true, true) );
+		echo $output = Common_Display("dump/flux_courant.xml", 25, true, true, true);
 	}
 	else
-		echo "Pas de contenu affichable.";
-
-	?>
+		echo "<br><br><h2>Pas de contenu affichable.</h2>";
+	
+	
+?>
