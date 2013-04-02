9394ec7d31b755682ecf08266dc36f8abc5b6dfd
ati.gastondefferre@gmail.com <ati.gastondefferre@gmail.com@98ed3dca-a519-a6af-8faf-6aacfb961b17>
2013-04-02 15:07:52 +0000

    problème de codage des caractères

--- gespac/gestion_prets/post_prets.php
+++ gespac/gestion_prets/post_prets.php
@@ -2,7 +2,7 @@
 
 	/* 
 	
-	fichier pour preter ou rendre du matériel
+	fichier pour preter ou rendre du matÃ©riel
 	
 	*/
 
@@ -13,12 +13,12 @@
 	include_once ('../../class/Log.class.php');	
 	include_once ('../../class/Sql.class.php');	
 
-	// Connexion à la base de données GESPAC
+	// Connexion Ã  la base de donnÃ©es GESPAC
 	$con_gespac = new Sql($host, $user, $pass, $gespac);
 	$log = new Log ("../dump/log_sql.sql");
 	
 	
-	// on récupère les paramètres de l'url	
+	// on rÃ©cupÃ¨re les paramÃ¨tres de l'url	
 	$action 	= $_GET['action'];
 	
 	
@@ -41,27 +41,27 @@
 		$req_preter_materiel = "UPDATE materiels SET user_id = $userid WHERE mat_id =$matid ;";
 		$con_gespac->Execute ( $req_preter_materiel );
 		
-		//on log la requête SQL
+		//on log la requÃªte SQL
 		$log->Insert($req_preter_materiel);
 		
-		//On récupère le nom d'utilisateur en fonction du user_id
+		//On rÃ©cupÃ¨re le nom d'utilisateur en fonction du user_id
 		$user_nom = $con_gespac->QueryOne ( "SELECT user_nom FROM users WHERE user_id = $userid" );
 		
-		//On récupère le nom de matériel en fonction du mat_id
+		//On rÃ©cupÃ¨re le nom de matÃ©riel en fonction du mat_id
 		$liste_materiel = $con_gespac->QueryRow ( "SELECT mat_nom, mat_serial FROM materiels WHERE mat_id = $matid" );
 		$mat_nom = $liste_materiel[0];
 		$mat_serial = $liste_materiel[1];
 		
-		echo $log_texte = "Le matériel $mat_nom (Numéro de série : <b>$mat_serial</b>) a été prêté à $user_nom";
+		echo $log_texte = "Le matÃ©riel $mat_nom (NumÃ©ro de sÃ©rie : <b>$mat_serial</b>) a Ã©tÃ© prÃªtÃ© Ã  $user_nom";
 				
-		// On insère une ligne dans les logs pour tracer tout ça
-		$req_log_preter_materiel = "INSERT INTO logs ( log_type, log_texte ) VALUES ( 'Prêté', '$log_texte' );";
+		// On insÃ¨re une ligne dans les logs pour tracer tout Ã§a
+		$req_log_preter_materiel = "INSERT INTO logs ( log_type, log_texte ) VALUES ( 'PrÃªtÃ©', '$log_texte' );";
 		$con_gespac->Execute ( $req_log_preter_materiel );
 		
-		//on log la requête SQL
+		//on log la requÃªte SQL
 		$log->Insert($req_log_preter_materiel);
 				
-		// On ouvre une autre fenêtre pour la convention de pret
+		// On ouvre une autre fenÃªtre pour la convention de pret
 		echo "<script type='text/javascript'>window.open(\"gestion_prets/convention_pret.php?matid=$matid&userid=$userid\", 'CONVENTION DE PRET');</script>";
 		
 		
@@ -75,31 +75,31 @@
 		$userid		= $_GET['userid'];
 	
 		
-		// On récupère le nom de l'utilisateur en fonction du user_id
+		// On rÃ©cupÃ¨re le nom de l'utilisateur en fonction du user_id
 		$user_nom = $con_gespac->QueryOne ( "SELECT user_nom FROM users WHERE user_id = $userid" );
 		
 		
-		// On récupère le nom du matériel en fonction du mat_id
+		// On rÃ©cupÃ¨re le nom du matÃ©riel en fonction du mat_id
 		$liste_materiel = $con_gespac->QueryRow ( "SELECT mat_nom, mat_serial FROM materiels WHERE mat_id = $matid" );
 		$mat_nom = $liste_materiel [0];
 		$mat_serial = $liste_materiel [1];
 		
-		$log_texte = urlencode("<a href='./gestion_prets/convention_retour.php?matid=$matid&userid=$userid&id_conv=0' target=_blank>$user_nom a rendu le matériel $mat_nom (Numéro de série : <b>$mat_serial</b>)</a>");
+		$log_texte = urlencode("<a href='./gestion_prets/convention_retour.php?matid=$matid&userid=$userid&id_conv=0' target=_blank>$user_nom a rendu le matÃ©riel $mat_nom (NumÃ©ro de sÃ©rie : <b>$mat_serial</b>)</a>");
 		
-		// On insère une ligne dans les logs pour tracer tout ça
+		// On insÃ¨re une ligne dans les logs pour tracer tout Ã§a
 		$req_log_rendu_materiel = "INSERT INTO logs ( log_type, log_texte ) VALUES ( 'Rendu', '$log_texte' );";
 		$con_gespac->Execute ( $req_log_rendu_materiel );
 		
-		//on log la requête SQL
+		//on log la requÃªte SQL
 		$log->Insert($req_log_rendu_materiel);
 
-		echo "Le matériel est rendu !";
+		echo "Le matÃ©riel est rendu !";
 				
-		// On ouvre une autre fenêtre pour la convention de retour
+		// On ouvre une autre fenÃªtre pour la convention de retour
 		echo "<script type='text/javascript'>window.open(\"gestion_prets/convention_retour.php?matid=$matid&userid=$userid&id_conv=1\", 'CONVENTION DE RETOUR');</script>";
 		
 	
-		// On ne rend pas ici le matériel mais dans le fichier convention_retour.php car la mise à jour de la table est trop rapide et les données n'existent plus lors de la création de la convention (donc convention vierge)
+		// On ne rend pas ici le matÃ©riel mais dans le fichier convention_retour.php car la mise Ã  jour de la table est trop rapide et les donnÃ©es n'existent plus lors de la crÃ©ation de la convention (donc convention vierge)
 						
 
 	}
