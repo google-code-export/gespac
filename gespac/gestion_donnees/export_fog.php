3ef840c9eb4f47475336154e89d2eeb0bccebcf2
ati.gastondefferre@gmail.com <ati.gastondefferre@gmail.com@98ed3dca-a519-a6af-8faf-6aacfb961b17>
2013-04-04 14:06:17 +0000

    Migration des exports.
    - Test d'existence des différentes base de données.
    - Si la base OCS n'est pas présente, on remplace les valeurs LASTCOME et FIDELITY par NA.
    - Correction sur le QueryAll de la liste d'import qui donnait des valeurs erronées.
    (avec les nouvelles classes SQL, ce n'est plus $record[0][0] mais $record[0]['clg_uai'])

--- gespac/gestion_donnees/export_fog.php
+++ gespac/gestion_donnees/export_fog.php
@@ -1,29 +1,33 @@
 <?PHP
 
-	// lib
-	require_once ('../config/databases.php');
-	require_once ('../fonctions.php');
-	include_once ('../../class/Sql.class.php');	
+	// Connexion √† la base de donn√©es GESPAC
+	$con_ocs = new Sql ($host, $user, $pass, $ocsweb);
+	
 
-	// Connexion ‡ la base de donnÈes GESPAC
-	$con_ocs = new Sql ( $host, $user, $pass, $ocsweb );
+	if ($con_ocs->Exists()) {
+		// stockage des lignes retourn√©es par sql dans un tableau (je ne r√©cup√®re que le matos associ√© √† une marque)
+		$liste_hardware = $con_ocs->QueryAll ( "SELECT macaddr, name FROM hardware, networks WHERE hardware.id = networks.hardware_id;" );
 
-	// stockage des lignes retournÈes par sql dans un tableau (je ne rÈcupËre que le matos associÈ ‡ une marque)
-	$liste_hardware = $con_ocs->queryAll ( "SELECT macaddr, name FROM hardware, networks WHERE hardware.id = networks.hardware_id;" );
+		$fp = fopen('dump/ocs_vers_fog.csv', 'w+');
 
-	$fp = fopen('../dump/ocs_vers_fog.csv', 'w+');
+		foreach ($liste_hardware as $record) {
+			$mac 	= $record['macaddr'];
+			$name 	= $record['name'];
+			
+			fputcsv($fp, array($mac, $name), ',' );	// les delimiters et "encloseurs" par defaut ne marchent pas ? tant pis
+		}
 
-	foreach ($liste_hardware as $record) {
-		$mac 	= $record['macaddr'];
-		$name 	= $record['name'];
-		
-		fputcsv($fp, array($mac, $name), ',' );	// les delimiters et "encloseurs" par defaut ne marchent pas ? tant pis
+		fclose($fp);
+		$con_ocs->Close();
+
+
+		?>
+
+		<center><h1><a href="dump/ocs_vers_fog.csv">Fichier CSV OCS pour FOG</a></h1></center>
+
+<?PHP
 	}
-
-	fclose($fp);
-	$con_ocs->Close();
-
-
+	else {
+		echo "<center><h2 style='color:red;'>La base OCS ne semble pas joignable. Impossible de cr√©er le fichier d'export pour FOG.</h2></center>";
+	}
 ?>
-
-<center><h1><a href="./dump/ocs_vers_fog.csv">Fichier CSV OCS pour FOG</a></h1></center>
