93782eaec0712063547aa512af0427d73645ad4a
ati.gastondefferre@gmail.com <ati.gastondefferre@gmail.com@98ed3dca-a519-a6af-8faf-6aacfb961b17>
2013-04-05 08:35:52 +0000

    Une fois cliqué, le sous menu reste ouvert.

--- gespac/js/main.js
+++ gespac/js/main.js
@@ -19,6 +19,19 @@
 	};
 
 
+	// Pour parser la chaine get en js
+	function getQueryVariable(variable) {
+		var query = window.location.search.substring(1);
+		var vars = query.split('&');
+		for (var i = 0; i < vars.length; i++) {
+			var pair = vars[i].split('=');
+			if (decodeURIComponent(pair[0]) == variable) {
+				return decodeURIComponent(pair[1]);
+			}
+		}
+	}
+
+
 window.addEvent('domready', function(){
 	
 	// On implémente la fonctionnalité show/hide/toggle
--- gespac/menu.php
+++ gespac/menu.php
@@ -21,7 +21,7 @@
 		<div class="menu-block" id="menu-accueil">
 			<div class="menu-titre" id="entete-menu-accueil"><img src="img/icons/accueil.png">ACCUEIL</div>
 			<div class="menu-items">
-				<a href="index.php?page=accueil"><div class="menu-item">Retour à l'accueil</div></a>
+				<a href="index.php?page=accueil"><div class="menu-item" id='accueil'>Retour à l'accueil</div></a>
 				<a href="../index.php"><div class="menu-item">Retour au portail</div></a>
 			</div>
 		</div>
@@ -30,9 +30,9 @@
 		<div class="menu-block" id="menu-inventaire">
 			<div class="menu-titre" id="entete-menu-inventaire"><img src="img/icons/inventaire.png">INVENTAIRE</div>
 			<div class="menu-items">
-				<a href="index.php?page=materiels"><div class="menu-item">matériels</div></a>
-				<a href="index.php?page=marques"><div class="menu-item">marques</div></a>
-				<a href="index.php?page=salles"><div class="menu-item">salles</div></a>
+				<a href="index.php?page=materiels"><div class="menu-item" id='materiels' >matériels</div></a>
+				<a href="index.php?page=marques"><div class="menu-item" id='marques'>marques</div></a>
+				<a href="index.php?page=salles"><div class="menu-item" id='salles'>salles</div></a>
 			</div>
 		</div>
 		
@@ -40,7 +40,7 @@
 		<div class="menu-block" id="menu-dossiers">
 			<div class="menu-titre" id="entete-menu-dossiers"><img src="img/icons/dossiers.png">DOSSIERS</div>
 			<div class="menu-items">
-				<a href="index.php?page=dossiers"><div class="menu-item">Gérer les dossiers</div></a>
+				<a href="index.php?page=dossiers"><div class="menu-item" id='dossiers'>Gérer les dossiers</div></a>
 			</div>
 		</div>
 	
@@ -48,7 +48,7 @@
 		<div class="menu-block" id="menu-prets">
 			<div class="menu-titre" id="entete-menu-prets"><img src="img/icons/pret.png">PRETS</div>
 			<div class="menu-items">
-				<a href="index.php?page=prets"><div class="menu-item">Gérer les prêts</div></a>
+				<a href="index.php?page=prets"><div class="menu-item" id='prets'>Gérer les prêts</div></a>
 			</div>
 		</div>
 		
@@ -56,51 +56,51 @@
 		<div class="menu-block" id="menu-users">
 			<div class="menu-titre" id="entete-menu-users"><img src="img/icons/users.png">UTILISATEURS</div>
 			<div class="menu-items">
-				<a href="index.php?page=utilisateurs"><div class="menu-item">Visualiser les utilisateurs</div></a>
-				<a href="index.php?page=grades"><div class="menu-item">Visualiser les grades</div></a>
-				<a href="index.php?page=importiaca"><div class="menu-item">Importer les comptes IACA</div></a>
-				<a href="index.php?page=moncompte"><div class="menu-item">Modifier mon compte</div></a>
+				<a href="index.php?page=utilisateurs"><div class="menu-item" id='utilisateurs'>Visualiser les utilisateurs</div></a>
+				<a href="index.php?page=grades"><div class="menu-item" id='grades'>Visualiser les grades</div></a>
+				<a href="index.php?page=importiaca"><div class="menu-item" id='importiaca'>Importer les comptes IACA</div></a>
+				<a href="index.php?page=moncompte"><div class="menu-item" id='moncompte'>Modifier mon compte</div></a>
 			</div>
 		</div>
 		
 		<div class="menu-block" id="menu-modules">
 			<div class="menu-titre" id="entete-menu-modules"><img src="img/icons/modules.png">MODULES</div>
 			<div class="menu-items">
-				<a href="index.php?page=recapfog"><div class="menu-item">Récapitulatif FOG</div></a>
-				<a href="index.php?page=wol"><div class="menu-item">Wake On Lan</div></a>
-				<a href="index.php?page=exportsperso"><div class="menu-item">Export Perso</div></a>
-				<a href="index.php?page=taginventaire"><div class="menu-item">MAJ No Inventaire</div></a>
-				<a href="index.php?page=imagefog"><div class="menu-item">Images Fog</div></a>
-				<a href="index.php?page=modportail"><div class="menu-item">Menu portail</div></a>
-				<a href="index.php?page=gestfichiers"><div class="menu-item">Gestionnaire de fichiers</div></a>
-				<a href="index.php?page=migfog"><div class="menu-item">Migration Fog</div></a>
-				<a href="index.php?page=migdossiers"><div class="menu-item">Migration dossiers</div></a>
-				<a href="index.php?page=geninventaire"><div class="menu-item">Générer Inventaire</div></a>
-				<a href="index.php?page=migusers"><div class="menu-item">Migration Utilisateurs</div></a>
-				<a href="index.php?page=aic"><div class="menu-item">Création AIC</div></a>
+				<a href="index.php?page=recapfog"><div class="menu-item" id='recapfog'>Récapitulatif FOG</div></a>
+				<a href="index.php?page=wol"><div class="menu-item" id='wol'>Wake On Lan</div></a>
+				<a href="index.php?page=exportsperso"><div class="menu-item" id='exportsperso'>Export Perso</div></a>
+				<a href="index.php?page=taginventaire"><div class="menu-item" id='taginventaire'>MAJ No Inventaire</div></a>
+				<a href="index.php?page=imagefog"><div class="menu-item" id='imagefog'>Images Fog</div></a>
+				<a href="index.php?page=modportail"><div class="menu-item" id='modportail'>Menu portail</div></a>
+				<a href="index.php?page=gestfichiers"><div class="menu-item" id='gestfichiers'>Gestionnaire de fichiers</div></a>
+				<a href="index.php?page=migfog"><div class="menu-item" id='migfog'>Migration Fog</div></a>
+				<a href="index.php?page=migdossiers"><div class="menu-item" id='migdossiers'>Migration dossiers</div></a>
+				<a href="index.php?page=geninventaire"><div class="menu-item" id='geninventaire'>Générer Inventaire</div></a>
+				<a href="index.php?page=migusers"><div class="menu-item" id='migusers'>Migration Utilisateurs</div></a>
+				<a href="index.php?page=aic"><div class="menu-item" id='aic'>Création AIC</div></a>
 			</div>
 		</div>
 			
 		<div class="menu-block" id="menu-info">
 			<div class="menu-titre" id="entete-menu-info"><img src="img/icons/info.png">INFO</div>
 			<div class="menu-items">
-				<a href="index.php?page=college"><div class="menu-item">Fiche collège</div></a>
-				<a href="index.php?page=rss"><div class="menu-item">Flux RSS</div></a>
-				<a href="index.php?page=statbat"><div class="menu-item">Stats bâtons</div></a>
-				<a href="index.php?page=statparc"><div class="menu-item">Stats utilisation du parc</div></a>
-				<a href="index.php?page=infoserveur"><div class="menu-item">Info serveur</div></a>
+				<a href="index.php?page=college"><div class="menu-item" id='college'>Fiche collège</div></a>
+				<a href="index.php?page=rss"><div class="menu-item" id='rss'>Flux RSS</div></a>
+				<a href="index.php?page=statbat"><div class="menu-item" id='statbat'>Stats bâtons</div></a>
+				<a href="index.php?page=statparc"><div class="menu-item" id='statparc'>Stats utilisation du parc</div></a>
+				<a href="index.php?page=infoserveur"><div class="menu-item" id='infoserveur'>Info serveur</div></a>
 			</div>
 		</div>
 		
 		<div class="menu-block" id="menu-donnees">
 			<div class="menu-titre" id="entete-menu-donnees"><img src="img/icons/data.png">DONNEES</div>
 			<div class="menu-items">
-				<a href="index.php?page=importocs"><div class="menu-item">Importer DB OCS</div></a>
-				<a href="index.php?page=exports"><div class="menu-item">Exports</div></a>
-				<a href="index.php?page=dumpgespac"><div class="menu-item">Dump base GESPAC</div></a>
-				<a href="index.php?page=dumpocs"><div class="menu-item">Dump base OCS</div></a>
-				<a href="index.php?page=logs"><div class="menu-item">Voir les Logs</div></a>
-				<a href="index.php?page=importcsv"><div class="menu-item">Importer CSV</div></a>
+				<a href="index.php?page=importocs"><div class="menu-item" id='importocs'>Importer DB OCS</div></a>
+				<a href="index.php?page=exports"><div class="menu-item" id='exports'>Exports</div></a>
+				<a href="index.php?page=dumpgespac"><div class="menu-item" id='dumpgespac'>Dump base GESPAC</div></a>
+				<a href="index.php?page=dumpocs"><div class="menu-item" id='dumpocs'>Dump base OCS</div></a>
+				<a href="index.php?page=logs"><div class="menu-item" id='logs'>Voir les Logs</div></a>
+				<a href="index.php?page=importcsv"><div class="menu-item" id='importcsv'>Importer CSV</div></a>
 			</div>
 		</div>
 	
@@ -135,6 +135,12 @@
 				$$('.menu-items').setStyle("display", "none");	// On masque tout
 				$(this).getParent().getChildren(".menu-items").setStyle("display", "block");	// On affiche le set d'items
 			});
+			
+			var page = getQueryVariable("page");
+			if (page) {
+				$(page).getParent(".menu-items").setStyle("display", "block");	// On affiche le set d'items
+				$(page).className='menu-current';
+			}
 		
 		});
 		
