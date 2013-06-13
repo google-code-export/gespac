f030003431b06a674c9c9af4d8c8ab8dc9447d61
ati.gastondefferre@gmail.com <ati.gastondefferre@gmail.com@98ed3dca-a519-a6af-8faf-6aacfb961b17>
2013-06-13 09:18:19 +0000

    Problème de création des exports si la base ocs est absente.

--- branches/3.4/gespac/gestion_donnees/export_fog.php
+++ branches/3.4/gespac/gestion_donnees/export_fog.php
@@ -7,23 +7,32 @@
 
 	// Connexion à la base de données GESPAC
 	$con_ocs = new Sql ( $host, $user, $pass, $ocsweb );
+	
+	if ($con_ocs->Exists()) {
+					
+		// stockage des lignes retournées par sql dans un tableau (je ne récupère que le matos associé à une marque)
+		$liste_hardware = $con_ocs->queryAll ( "SELECT macaddr, name FROM hardware, networks WHERE hardware.id = networks.hardware_id;" );
 
-	// stockage des lignes retournées par sql dans un tableau (je ne récupère que le matos associé à une marque)
-	$liste_hardware = $con_ocs->queryAll ( "SELECT macaddr, name FROM hardware, networks WHERE hardware.id = networks.hardware_id;" );
+		$fp = fopen('../dump/ocs_vers_fog.csv', 'w+');
 
-	$fp = fopen('../dump/ocs_vers_fog.csv', 'w+');
+		foreach ($liste_hardware as $record) {
+			$mac 	= $record['macaddr'];
+			$name 	= $record['name'];
+			
+			fputcsv($fp, array($mac, $name), ',' );	// les delimiters et "encloseurs" par defaut ne marchent pas ? tant pis
+		}
 
-	foreach ($liste_hardware as $record) {
-		$mac 	= $record['macaddr'];
-		$name 	= $record['name'];
-		
-		fputcsv($fp, array($mac, $name), ',' );	// les delimiters et "encloseurs" par defaut ne marchent pas ? tant pis
-	}
-
-	fclose($fp);
-	$con_ocs->Close();
+		fclose($fp);
+		$con_ocs->Close();
 
 
 ?>
 
-<center><h1><a href="./dump/ocs_vers_fog.csv">Fichier CSV OCS pour FOG</a></h1></center>
+	<center><h1><a href="./dump/ocs_vers_fog.csv">Fichier CSV OCS pour FOG</a></h1></center>
+
+<?php
+	}
+	else {
+		echo "<center><h2 style='color:red;'>La base OCS ne semble pas joignable. Impossible de créer le fichier d'export pour FOG.</h2></center>";
+	}
+?>
